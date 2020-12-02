69c5f0c34916ddbb79984870ee46a118389cb316
Cakemix <mholoch@gmail.com>
2020-12-02 19:13:47 +0100

    finished pw validity check

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -3,23 +3,27 @@ type Password = String;
 #[derive(Debug, PartialEq)]
 struct Policy {
     required_char: char,
-    required_min: u32,
-    required_max: u32,
+    required_min: usize,
+    required_max: usize,
 }
 
 #[derive(Debug, PartialEq)]
-struct PasswordWithPolicy {
+pub struct PasswordWithPolicy {
     policy: Policy,
     password: Password,
 }
 
 impl PasswordWithPolicy {
-    fn from_string(password_with_policy_string: &'static str) -> PasswordWithPolicy {
+    pub fn from_string(password_with_policy_string: &'static str) -> PasswordWithPolicy {
         let mut pw_with_pol_string_parts = password_with_policy_string.split(": ");
         PasswordWithPolicy {
             policy: Policy::from_string(pw_with_pol_string_parts.next().unwrap()),
             password: pw_with_pol_string_parts.next().unwrap().to_string(),
         }
+    }
+    pub fn is_valid(self) -> bool {
+        let occurrence_count = self.password.split(self.policy.required_char).collect::<Vec<&str>>().len() - 1; // -1, because the one element will always exist even if there's no occurrence of the required_char
+        return  self.policy.required_min <= occurrence_count && occurrence_count <= self.policy.required_max;
     }
 }
 
@@ -40,6 +44,17 @@ fn main() {
 }
 
 #[test]
+fn test_part1_simple_example() {
+    let test_strings = ["1-3 a: abcde", "1-3 b: cdefg", "2-9 c: ccccccccc"];
+    let p0 = PasswordWithPolicy::from_string(test_strings[0]);
+    let p1 = PasswordWithPolicy::from_string(test_strings[1]);
+    let p2 = PasswordWithPolicy::from_string(test_strings[2]);
+    assert_eq!(p0.is_valid(), true);
+    assert_eq!(p1.is_valid(), false);
+    assert_eq!(p2.is_valid(), true);
+}
+
+#[test]
 fn test_policy_from_string() {
     let p = Policy::from_string("1-3 a");
     assert_eq!(
@@ -53,7 +68,7 @@ fn test_policy_from_string() {
 }
 
 #[test]
-fn test_part1_simple_example() {
+fn test_password_with_policy_from_string() {
     let password_with_policy = PasswordWithPolicy::from_string("1-3 a: abcde");
     assert_eq!(
         password_with_policy,
